eca65c4cf77313312a6cd5dbe9b82f94068fbdc6
Mrigakshi Pandey <mrigakshipandey@gmail.com>
2022-07-31 18:43:32 +0530

    reduces boot time

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port = process.env.PORT || 3000;
-
-  app.listen(port, function() {
-    console.log("Server started on: ", port);
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Our app is running on port ${PORT}`);
   });
 
 }
