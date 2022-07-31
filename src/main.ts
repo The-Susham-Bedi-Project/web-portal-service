00d521995a13a3d0e7e132b9924a33bbfb42424b
Mrigakshi Pandey <mrigakshipandey@gmail.com>
2022-07-31 17:39:29 +0530

    changes host

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  const host = '0.0.0.0';
+  const port = process.env.PORT || 3000;
+  await app.listen(port, host);
 }
 bootstrap();
