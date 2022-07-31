428b483927cb35a93882061527a5ebf21eadd905
Mrigakshi Pandey <mrigakshipandey@gmail.com>
2022-07-31 18:22:57 +0530

    removes host

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const host = '0.0.0.0';
   const port = process.env.PORT || 3000;
 
-  app.listen(port, host, function() {
-    console.log("Server started.......");
+  app.listen(port, function() {
+    console.log("Server started on: ", port);
   });
-  
+
 }
 bootstrap();
