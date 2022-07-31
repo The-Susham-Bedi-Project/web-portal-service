34fafe9241d049ecc08e6613bc175fc6fca15c01
Mrigakshi Pandey <mrigakshipandey@gmail.com>
2022-07-31 17:44:35 +0530

    changes listen call

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ async function bootstrap() {
   app.enableCors();
   const host = '0.0.0.0';
   const port = process.env.PORT || 3000;
-  await app.listen(port, host);
+
+  app.listen(port, host, function() {
+    console.log("Server started.......");
+  });
+  
 }
 bootstrap();
