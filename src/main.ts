ace64233a2189aea96764b2bfa8cb52f073691a7
Mrigakshi Pandey <mrigakshipandey@gmail.com>
2022-07-31 17:10:26 +0530

    changes port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d", this.address().port);
+  });
 }
 bootstrap();
